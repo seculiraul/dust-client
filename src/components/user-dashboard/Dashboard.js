e000b73c9fb36b6f6003644696a6c3b1b46c5727
Raul <raulsmarius@yahoo.com>
2023-08-23 21:09:17 +0300

    Changed User Name in Top of Application And Account Details

--- src/components/user-dashboard/Dashboard.js
+++ src/components/user-dashboard/Dashboard.js
@@ -2,30 +2,27 @@ import { Link, matchPath, Outlet, useLocation } from 'react-router-dom'
 
 const Dashboard = () => {
   const path = useLocation().pathname
-  const a = '2023-04-22T08:44:05.804Z'.console.log(a)
+
+  const pathActive = (currentPath) => {
+    return path === currentPath
+      ? 'border-b border-b-gray-800 font-bold'
+      : 'hover:border-b hover:border-b-gray-400'
+  }
   return (
     <div>
-      <div className="flex flex-row p-2 m-2 md:ml-36 justify-start items-start sapce-x-2">
-        <div className="flex flex-col justify-start items-start p-2 m-2 ">
-          <Link
-            to="/user/details"
-            className={
-              path == '/user/details' ? 'border-b border-b-gray-600' : ''
-            }
-          >
+      <div className="flex flex-row p-2 m-2 md:ml-36 md:mr-12 justify-start items-start sapce-x-2">
+        <div className="flex flex-col justify-start items-start gap-2 p-2 m-2 w-1/6 rounded-md shadow-sm">
+          <Link to="/user/details" className={pathActive('/user/details')}>
             Account details
           </Link>
-          <Link
-            to="/user/orders"
-            className={
-              path == '/user/orders' ? 'border-b border-b-gray-600' : ''
-            }
-          >
+          <Link to="/user/orders" className={pathActive('/user/orders')}>
             Orders
           </Link>
-          <button>Logout</button>
+          <button className={pathActive('/logout')}>Logout</button>
         </div>
-        <Outlet />
+        <div className="border-l border-l-gray-300">
+          <Outlet />
+        </div>
       </div>
     </div>
   )
