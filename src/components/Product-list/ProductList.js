c984263e355d665feab0e4deb70af2823b65ba42
Raul <raulsmarius@yahoo.com>
2023-08-28 22:32:59 +0300

    Small Changes And Fix Some Warnings

--- src/components/Product-list/ProductList.js
+++ src/components/Product-list/ProductList.js
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router'
-import { Link } from 'react-router-dom'
 
 const ProductList = ({ products }) => {
   const navigate = useNavigate()
@@ -13,7 +12,7 @@ const ProductList = ({ products }) => {
 
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.code} className="group">
+            <div key={product.code} className="group">
               <div
                 style={{ cursor: 'pointer' }}
                 className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8"
@@ -29,7 +28,7 @@ const ProductList = ({ products }) => {
               <p className="mt-1 text-lg font-medium text-gray-900">
                 {product.price}
               </p>
-            </a>
+            </div>
           ))}
         </div>
       </div>
