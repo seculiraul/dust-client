b2104bf12cba00e6fe8d42516a9b57919db11ac0
Raul <raulsmarius@yahoo.com>
2023-09-04 22:26:08 +0300

    Adjusted Slides

--- src/components/home/RecomandedProductsSlide.js
+++ src/components/home/RecomandedProductsSlide.js
@@ -1,12 +1,15 @@
+import { SwiperSlide } from 'swiper/react'
 const { default: ProductCard } = require('../shared/PtroductCard')
 const { default: SlideShow } = require('./SlideShow')
 
 const RecomandedProductsSlide = ({ recomandedProducts }) => {
   return (
-    <SlideShow>
-      {recomandedProducts?.map((product) => {
-        return <ProductCard key={product?.code} product={product} />
-      })}
+    <SlideShow slidesPerView={3} navigation pagination={false}>
+      {recomandedProducts?.map((product, index) => (
+        <SwiperSlide key={index}>
+          <ProductCard key={product?.code} product={product} />
+        </SwiperSlide>
+      ))}
     </SlideShow>
   )
 }
