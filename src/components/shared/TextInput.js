e5d0e9e2c407f4d98f244f4d6655b7a80d440522
Raul <raulsmarius@yahoo.com>
2023-09-10 14:11:01 +0300

    Added Product Creation Form And Extended Product Api

--- src/components/shared/TextInput.js
+++ src/components/shared/TextInput.js
@@ -1,9 +1,9 @@
-const TextInput = ({ extraClasses, placeholder }) => {
+const TextInput = ({ extraClasses, ...rest }) => {
   return (
     <input
       className={`p-2 border-2 border-slate-300 rounded-md focus:bg-gray-100 hover:border-gray-400 focus:border-gray-400 focus:outline-0 duration-200 ${extraClasses}`}
       type="text"
-      placeholder={placeholder}
+      {...rest}
     />
   )
 }
